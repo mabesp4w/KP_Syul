4957005e7ebc0f444c314cdabdf437229b1dce21
MabesP4W <mabesp4w@gmail.com>
2025-11-25 07:37:40 +0900

    tambah build pada git

---
+++ public/build/assets/AOSWrapper-CyGhR2Oq.js
@@ -0,0 +1,1 @@
+import{u as t,r as a,d as o,j as r}from"./app-DtJ35xiz.js";function n({children:s}){const{url:e}=t();return a.useEffect(()=>{o.refresh()},[e]),r.jsx(r.Fragment,{children:s})}export{n as A};
